8512ecdde7d45281e83d9271a81bf8d868bf72bc
ElaineGxy <gaoxiyan2011@163.com>
2016-07-03 11:51:35 +0800

    创建新问题

--- Handler/createTopic.php
+++ Handler/createTopic.php
@@ -12,7 +12,7 @@
    //mysql_connect
    $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS,$DB_DATABASENAME);
    //读取表中纪录条数
-   $sql = sprintf("insert into %s (subject, content, signupnum, sponsorID, receiverID, starttime, validate,tag, type) values ('%s','%s', 0, '%s', 0, ' ', -1,'%s', '%s')", $DB_TABLENAME, $subject,$content,$sponsor,$tag,$type);
+   $sql = sprintf("insert into %s (subject, content, signupnum, sponsorID, receiverID, starttime, validate,tag, type) values ('%s','%s', 0, '%s', 0, ' ', 1,'%s', '%s')", $DB_TABLENAME, $subject,$content,$sponsor,$tag,$type);
 
     if(!mysqli_query($conn,$sql)){
         echo "fail";
