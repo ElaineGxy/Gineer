2b880fd485b0ea989bc7864644d2bedf9c9932d0
ElaineGxy <gaoxiyan2011@163.com>
2016-07-03 10:26:02 +0800

    创建新问题

--- Handler/createTopic.php
+++ Handler/createTopic.php
@@ -7,17 +7,21 @@
   $content = $_POST['content'];
   $sponsor = $_POST['sponsorID']; // sponsor id
   $type = $_POST['type'];
+  $tag=$_POST['tag'];
 
    //mysql_connect
    $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS,$DB_DATABASENAME);
    //读取表中纪录条数
-   $sql = sprintf("insert into %s (subject, content, signupnum, sponsorID, receiverID, starttime, validate, type) values ('%s','%s', 0, '%s', 0, ' ', -1, 0)", $DB_TABLENAME, $subject,$content,$sponsor);
-    
+   $sql = sprintf("insert into %s (subject, content, signupnum, sponsorID, receiverID, starttime, validate,tag, type) values ('%s','%s', 0, '%s', 0, ' ', -1,'%s', '%s')", $DB_TABLENAME, $subject,$content,$sponsor,$tag,$type);
+
     if(!mysqli_query($conn,$sql)){
-        die('Error:');
+        echo "fail";
+    }else{
+      $topic_id=mysqli_insert_id($conn);
+     echo "success";
     }
-    $topic_id=mysqli_insert_id($conn);
     mysqli_close($conn);
-    echo $topic_id;
+
+    
 ?>
 
